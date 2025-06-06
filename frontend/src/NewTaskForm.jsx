02da3120c36cdb51b9f5234348b65c32308426db
Talha Salahuddin <59451858+talhaayyubi@users.noreply.github.com>
2025-06-06 15:44:19 +0500

    Add NewTaskForm and refresh mechanism

---
+++ frontend/src/NewTaskForm.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import axios from 'axios'
+
+function NewTaskForm({ onTaskCreated }) {
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    axios
+      .post('/api/tasks', { title, description })
+      .then(() => {
+        setTitle('')
+        setDescription('')
+        if (onTaskCreated) {
+          onTaskCreated()
+        }
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>
+          Title:
+          <input value={title} onChange={(e) => setTitle(e.target.value)} />
+        </label>
+      </div>
+      <div>
+        <label>
+          Description:
+          <input
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </label>
+      </div>
+      <button type="submit">Add Task</button>
+    </form>
+  )
+}
+
+export default NewTaskForm
+
